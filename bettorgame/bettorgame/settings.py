0ee95b57f0fb603bd7001a4e55d033973a195531
jiangl <feefifofisa@gmail.com>
2020-04-05 18:01:47 -0400

    update to postgres

--- bettorgame/bettorgame/settings.py
+++ bettorgame/bettorgame/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'bettorgame.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bettorgamedb',
+        'USER': 'vardo',
+        'PASSWORD': 'notsecure',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
