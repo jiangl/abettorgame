542341449b891b8c3260c297aa8f523caf9f9705
jcurhan <justin.curhan@gmail.com>
2020-04-06 18:12:00 -0400

    add template structure; login / create account page

--- bettorgame/bettorgame/settings.py
+++ bettorgame/bettorgame/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'maingame'
+    'maingame',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
--- bettorgame/maingame/views.py
+++ bettorgame/maingame/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Testing: Index Page")
+    return render(request, 'index.html', {'data': "test"})
