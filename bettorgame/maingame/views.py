2efd08d25659ddb4a69493d75e6f4a96d57c94ab
jcurhan <justin.curhan@gmail.com>
2020-04-06 19:27:40 -0400

    create index page, add bets

--- bettorgame/maingame/views.py
+++ bettorgame/maingame/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {'data': "test"})
+    return render(request, 'index.html', {'group': {'name': "Justin's Group", 'participants': ['justin', 'lisa', 'brendan', 'lily', 'colin', 'kait', 'ellen', 'senem', 'nick', ' dave', 'john', 'peter'], 'admin': 'justin'}})
