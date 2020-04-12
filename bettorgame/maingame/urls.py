a8fc270206eab82c17d2a45912d3ffab608e74aa
jcurhan <justin.curhan@gmail.com>
2020-04-11 20:09:35 -0400

    wip connecting templ/view/models

--- bettorgame/maingame/urls.py
+++ bettorgame/maingame/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login, name='login'),
+    path('join-group', views.join_group, name='join_group'),
+    path('create-group', views.create_group, name='create_group'),
     path('group-admin', views.group_admin, name='group_admin'),
     path('add-bets', views.add_bets, name='add_bets'),
     path('show-placements', views.show_placements, name='show_placements'),
