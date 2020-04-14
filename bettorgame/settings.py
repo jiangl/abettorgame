fc8ab9d20386348973065c4db7d6efe6f5bb2714
jiangl <feefifofisa@gmail.com>
2020-04-14 00:03:01 -0400

    add db

--- bettorgame/settings.py
+++ bettorgame/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,6 +77,8 @@ WSGI_APPLICATION = 'bettorgame.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -86,7 +89,9 @@ DATABASES = {
         'PORT': ''
     }
 }
-
+# heroku db
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
