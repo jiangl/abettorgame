8acf3b7e6349121cb3638a24855560b9bf60041c
jcurhan <justin.curhan@gmail.com>
2020-04-13 23:13:20 -0400

    update url, view, template logic for join/create group, admin page, set stakes, start event, end event

--- bettorgame/maingame/urls.py
+++ bettorgame/maingame/urls.py
@@ -5,10 +5,13 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login, name='login'),
-    path('join-group', views.join_group, name='join_group'),
-    path('create-group', views.create_group, name='create_group'),
-    path('group-admin', views.group_admin, name='group_admin'),
-    path('add-bets', views.add_bets, name='add_bets'),
+    path('join-group-and-event', views.join_group_and_event, name='join_group_and_event'),
+    path('create-group-and-event', views.create_group_and_event, name='create_group_and_event'),
+    path('<int:group_id>/<int:event_id>/group-admin', views.group_admin, name='group_admin'),
+    path('<int:group_id>/<int:event_id>/set-stakes', views.set_stakes, name='set_stakes'),
+    path('<int:group_id>/<int:event_id>/add-bets', views.add_bets, name='add_bets'),
+    path('<int:group_id>/<int:event_id>/start-event', views.start_event, name='start_event'),
+    path('<int:group_id>/<int:event_id>/end-event', views.end_event, name='end_event'),
     path('show-placements', views.show_placements, name='show_placements'),
     path('leaderboard', views.leaderboard, name='leaderboard'),
 ]
--- bettorgame/maingame/views.py
+++ bettorgame/maingame/views.py
@@ -1,48 +1,114 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import User, Group, Event, UserGroupRole, UserEventRole, Bet, BetOption, Placement, Result, EventType, StatusType, UserRole
-
+from django.contrib import messages
+import datetime
+import pytz
 import pdb
 
-# Test data
-group = {}
-user = {}
-group['name'] = "Justin's 'Friends'"
-group['id'] = "ABCD"
-group['started'] = True
-group['ended'] = False
-group['participants'] = ['justin', 'lisa', 'brendan', 'lily', 'colin', 'kait', 'ellen', 'senem', 'nick', ' dave', 'john', 'peter']
-group['admin'] = 'justin'
-group['stakes'] = 'First round of beers at the bar'
-group['bets'] = [{"creator": "lisa", "bet": "will the DJ play wagon wheel", "option_1": "Yes", "option_2": "No"}, {"creator": "justin", "bet": "who will fall first off of the chairlift?", "option_1": "Lisa", "option_2": "Anyone else"}, {"creator": "colin", "bet": "Will we play slapcup", "option_1": "Yes", "option_2": "No"}, {"creator": "alexander the great long name don't care", "bet": "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.", "option_1": "Lorem ipsum text is a scrambled version of a passage in classical Latin derived from the Marcus Tullius Cicero's treatise on ethics, De Finibus Bonorum et Malorum, written in 54 BC. Lorem ipsum is a garbled version of that original Latin text, and is nonsensical.", "option_2": "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content."}]
-group['participants_placed'] = ['justin', 'lisa', 'brendan', 'lily', 'colin', 'kait', 'ellen']
-group['participants_awaiting'] = ['senem', 'nick', ' dave', 'john', 'peter']
-group['participant_standings'] = [{"name": "justin", "won": "5", "lost": "1"},{"name": "lisa", "won": "4", "lost": "2"},{"name": "lily", "won": "4", "lost": "2"},{"name": "colin", "won": "3", "lost": "3"},{"name": "kait", "won": "2", "lost": "4"},{"name": "ellen", "won": "2", "lost": "4"},{"name": "senem", "won": "0", "lost": "6"}]
-user['stats'] = {"won":"3", "lost": "3", "remaining":"4", "rank":"4"}
-current_user = 'justin' 
+#How to render this check to see if user is ADMIN on basetemplate / other pages?
+#UserEventRole.objects.get(user=request.user.id, event=event_id))
+#can this be done more intelligently with middleware?
 
-# Create your views here.
 def index(request):
-    #pdb.set_trace()
-    return render(request, 'index.html', {'group': group, 'user': user, 'current_user': current_user})
+  #Q: how can we apply this to every page without putting the logic in ever view
+  # if request.user.is_authenticated:
+  return render(request, 'index.html', {})
+  # else:
+  #   return render(request, 'login.html', {})
+
+def join_group_and_event(request):
+    try:
+      groupAndEventIdArray = request.POST["groupAndEventId"].split('-')
+      group = Group.objects.get(id=groupAndEventIdArray[0])
+      event = Event.objects.get(id=groupAndEventIdArray[1])
+    except (KeyError, Event.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'index.html', {'error_message': "There is no Group with that ID."})
+    else:
+      #connect user to group/event
+      request.session['is_admin'] = "ADMIN" == UserEventRole.objects.get(user=request.user.id, event=event.id)
+
+      return HttpResponseRedirect(reverse('add_bets', args=[group.id, event.id]))
+
+def create_group_and_event(request):
+    new_group = Group(name=request.POST["groupName"])
+    new_group.save()  
+
+    placeholder_future_start_date = datetime.datetime.now().replace(tzinfo=pytz.UTC) + datetime.timedelta(days=365)
+    placeholder_future_end_date = placeholder_future_start_date + datetime.timedelta(days=7)
+
+    new_event = Event(start_time=placeholder_future_start_date, end_time=placeholder_future_end_date, name=new_group.name)
+    new_event.save()
+
+    user = User.objects.get(id=request.user.id)
+
+    new_group.players.add(user)
+    new_event.players.add(user)
+    new_event.groups.add(new_group)
+
+    role_admin = UserRole.objects.get(id=1)
+
+    group_commissioner = UserGroupRole(user=user, group=new_group, role=role_admin)
+    group_commissioner.save()
+    
+    event_commissioner = UserEventRole(user=user, event=new_event, role=role_admin)
+    event_commissioner.save()
+
+    request.session['is_admin'] = True
+
+    return HttpResponseRedirect(reverse('group_admin', args=[new_group.id, new_event.id]))
 
 def login(request):
 
-    return render(request, 'login.html', {'group': group, 'user': user, 'current_user': current_user})
+    return render(request, 'login.html', {})
 
-def group_admin(request):
+def group_admin(request, group_id, event_id):
+    event = Event.objects.get(id=event_id)
+    group = Group.objects.get(id=group_id)
 
-    return render(request, 'group-admin.html', {'group': group, 'user': user, 'current_user': current_user})
+    is_event_started = event.start_time < datetime.datetime.now().replace(tzinfo=pytz.UTC)
+    is_event_ended = event.end_time < datetime.datetime.now().replace(tzinfo=pytz.UTC)
+    event_status = {'is_event_started': is_event_started, 'is_event_ended': is_event_ended}
 
-def add_bets(request):
+    return render(request, 'group-admin.html', {'event': event, 'group': group, 'event_status': event_status})
 
-    return render(request, 'add-bets.html', {'group': group, 'user': user, 'current_user': current_user})
+def set_stakes(request, group_id, event_id):
+    event = Event.objects.get(id=event_id)
+    # NEED TO UPDATE MODEL TO HAVE STAKES ON EVENT
+    # stakes = request.POST["setStakes"]
+    # event.stakes = stakes
+    return HttpResponseRedirect(reverse('add_bets', args=(group_id,event.id,)))
+
+def start_event(request, group_id, event_id):
+    event = Event.objects.get(id=event_id)
+    event.start_time = datetime.datetime.now()
+
+    return HttpResponseRedirect(reverse('group_admin', args=(group_id,event.id,)))
+
+def end_event(request, group_id, event_id):
+    event = Event.objects.get(id=event_id)
+
+    event.end_time = datetime.datetime.now().replace(tzinfo=pytz.UTC)
+
+    return HttpResponseRedirect(reverse('group_admin', args=(group_id,event.id,)))
+
+def add_bets(request, group_id, event_id):
+    event = Event.objects.get(id=event_id)
+
+    #admin_role = UserRole.objects.get(name="ADMIN")
+    #how to access the enum?
+    #event_commissioner = UserEventRole.objects.get(role="ADMIN", event=event.id)
+
+    player_first_names = [player.first_name for player in event.players.all()]
+
+    return render(request, 'add-bets.html', {'event': event, 'group_id': group_id, 'player_first_names': player_first_names}) #'event_commissioner': event_commissioner
 
 def show_placements(request):
 
-    return render(request, 'show-placements.html', {'group': group, 'user': user, 'current_user': current_user})
+    return render(request, 'show-placements.html', {})
 
 def leaderboard(request):
 
-    return render(request, 'leaderboard.html', {'group': group, 'user': user, 'current_user': current_user})
+    return render(request, 'leaderboard.html', {})
