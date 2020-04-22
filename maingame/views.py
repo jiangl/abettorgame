d320e55dc93355f8b13083a9977a24f1d112a9cd
jiangl <feefifofisa@gmail.com>
2020-04-22 03:24:16 -0400

    update use of User in views

--- maingame/views.py
+++ maingame/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from .models import User, Group, Event, UserGroupRole, UserEventRole, Bet, BetOption, Placement, EventResult, BetResult, EventType, StatusType, UserRole
+from maingame.models import Group, Event, UserGroupRole, UserEventRole, Bet, BetOption, Placement, EventResult, BetResult, EventType, StatusType, UserRole
+from django.contrib.auth.models import User
 from django.contrib import messages
 import datetime
 import pytz
